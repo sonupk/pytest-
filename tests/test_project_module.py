4699b1160721d8475170a3fab6e55cac36905ed4
sonupk <sonukamble1121@gmail.com>
2025-01-30 15:01:09 +0530

    added @pytest fixture

--- tests/test_project_module.py
+++ tests/test_project_module.py
@@ -1,19 +1,25 @@
 
+import pytest
 import math
+
 
 from my_project import project_module
 
 
+
+def test_something(my_data):
+    assert my_data == 42
+
+
+def test_sqrt():
+    num = 25
+    assert math.sqrt(num) == 5
 
 def test_rolling_average():
     result = project_module.rolling_average([1,2,3,4,5,6],3)
     assert result == [2.0,3.0,4.0,5.0]
 
 
-
-def test_sqrt():
-    num = 25
-    assert math.sqrt(num) == 5
 
 
     
